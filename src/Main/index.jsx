1724768fbdfb0f637f4a5a0e1416a3913a58542c
Borys Ślaski <borys.slaski@gmail.com>
2019-12-06 10:24:47 +0100

    kolory

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -57,118 +57,112 @@ class Main extends Component {
         const allPrice= dayPrice*numbDays;
         return (
             <div className="borderBox">
-            <div style={{fontSize:'xx-large', margin : '50px 0 40px 0', textAlign: 'center'}}> Cennik usług na sezon 2019</div>
+            <div style={{fontSize:'xx-large', margin : '50px 0 0 0', textAlign: 'center', paddingTop: '20px', color: 'white'}}>Kalkulator pobytu 2019</div>
             <div> </div>
             <section className="container">
             <div>
             <table className="zui-table">
                 <tbody>
                 <tr>
-                    <td>pobyt osoby dorosłej</td>
+                    <td>{' osoba dorosła'.toUpperCase()}</td>
                     <td ><strong>{odoroslaPrice} zł</strong></td>
                     <td > <button onClick={this.onButtonClickMinus.bind(this)} name="odorosla" value={odorosla}> - </button> </td>
                     <td> {odorosla}</td>
                     <td > <button onClick={this.onButtonClickPlus.bind(this)} name="odorosla" value={odorosla}> + </button> </td>
                 </tr>
                 <tr>
-                    <td> pobyt dzieci do lat 10</td>
+                    <td> {'dziecko do lat 10'.toUpperCase()}</td>
                     <td ><strong>{dzieciPrice} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="dzieci" value={dzieci}> - </button> </td>
                     <td>{dzieci} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="dzieci" value={dzieci}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>młodzież i studenci</td>
+                    <td>MŁODZIEŹ / STUDENT</td>
                     <td ><strong> {mlodziezPrice} zł</strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="mlodziez" value={mlodziez}> - </button> </td>
                     <td>{mlodziez} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="mlodziez" value={mlodziez}> + </button> </td>
                 </tr>
                 <tr>
-                    <td> samochód</td>
+                    <td> SAMOCHÓD</td>
                     <td ><strong>{autoPrice} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="auto" value={auto}> - </button> </td>
                     <td>{auto} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="auto" value={auto}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>bus /van sypialny</td>
+                    <td>BUS /VAN SYPIALNY</td>
                     <td ><strong>{busPrice} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="bus" value={bus}> - </button> </td>
                     <td>{bus} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="bus" value={bus}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>energia</td>
+                    <td>ENERGIA</td>
                     <td ><strong>{energiaPrice} zł</strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="energia" value={energia}> - </button> </td>
                     <td>{energia} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="energia" value={energia}> + </button> </td>
                 </tr>
                 <tr>
-                    <td> przyczepa z przedsionkiem</td>
+                    <td> PRZYCZEPA Z PRZEDSIONKIEM</td>
                     <td ><strong> {przyczepaPrice} zł</strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="przyczepa" value={przyczepa}> - </button> </td>
                     <td>{przyczepa} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="przyczepa" value={przyczepa}> + </button> </td>
                 </tr>
                 <tr>
-                    <td> autocaravan</td>
+                    <td> CAMPER</td>
                     <td ><strong>{camperPrice} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="camper" value={camper}> - </button> </td>
                     <td>{camper} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="camper" value={camper}> + </button> </td>
                 </tr>
-                </tbody>
-            </table>
-            </div>
-            <div>
-            <table  className="zui-table">
-                <tbody>
                 <tr>
-                    <td>namiot 2 osobowy</td>
+                    <td>NAMIOT 2 OSOBOWY</td>
                     <td ><strong>{namiot2Price} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="namiot2" value={namiot2}> - </button> </td>
                     <td>{namiot2} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="namiot2" value={namiot2}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>namiot 3,4 osobowy</td>
+                    <td>NAMIOT 4 OSOBOWY</td>
                     <td ><strong>{namiot4Price} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="namiot4" value={namiot4}> - </button> </td>
                     <td>{namiot4} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="namiot4" value={namiot4}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>namiot 5,6 osobowy</td>
+                    <td>NAMIOT 5/6 OSOBOWY</td>
                     <td ><strong>{namiot6Price} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="namiot6" value={namiot6}> - </button> </td>
                     <td>{namiot6} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="namiot6" value={namiot6}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>motocykl</td>
+                    <td>MOTOCYKL</td>
                     <td ><strong>{motorPrice} zł</strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="motor" value={motor}> - </button> </td>
                     <td>{motor} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="motor" value={motor}> + </button> </td>
                 </tr>
                 <tr>
-                    <td> pies,zwierzęta domowe</td>
+                    <td> PIES </td>
                     <td ><strong>{piesPrice} zł </strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="pies" value={pies}> - </button> </td>
                     <td>{pies} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="pies" value={pies}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>altanka, pawilon ogrodowy</td>
+                    <td>ALTANKA/PAWILON OGRODOWY</td>
                     <td ><strong>{altanaPrice} zł</strong></td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="altana" value={altana}> - </button> </td>
                     <td>{altana} </td>
                     <td> <button onClick={this.onButtonClickPlus.bind(this)} name="altana" value={altana}> + </button> </td>
                 </tr>
                 <tr>
-                    <td>Ilość dni pobytu ?</td>
+                    <td>ILOŚĆ DNI POBYTU ?</td>
                     <td > </td>
                     <td>  <button onClick={this.onButtonClickMinus.bind(this)} name="numbDays" value={numbDays}> - </button> </td>
                     <td>{numbDays} </td>
@@ -179,11 +173,11 @@ class Main extends Component {
                     <td colSpan={4}><strong>&nbsp;  {taxa} zł </strong></td>
                 </tr>
                 <tr>
-                    <td>Cena za dobę</td>
+                    <td>CENA ZA DOBĘ</td>
                     <td colSpan={4}> {dayPrice}</td>
                 </tr>
                 <tr>
-                    <td>Cena za cały pobyt</td>
+                    <td>CENA ZA CAŁY POBYT</td>
                     <td colSpan={4}> {allPrice}</td>
                 </tr>
                 </tbody>
